4bd26d698c4decd2381c687d5acccb0cfe52579d
Johnny Arcia <cosmicvanbuuren@hotmail.com>
2023-02-06 20:57:37 -0600

    SQLite

--- pruebaapiari/pruebaapiari/Controllers/AriController.cs
+++ pruebaapiari/pruebaapiari/Controllers/AriController.cs
@@ -1,12 +1,118 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 
 namespace pruebaapiari.Controllers
 {
+    public class WorkOrder
+    {
+        public WorkOrder() { }
+        public WorkOrder(int workOrderId, string userId, string clientNumber, string actorIdCard, string actorFullName, string actorPhone, string actorEmail, string actorAddress, string actorSecondaryAddress, string jobOrderNumber, DateTime jobOrderApplicationDate, string jobOrderPath, string jobOrderCycle, string jobOrderProperty, string jobOrderTome, string jobOrderFolio, int jobOrderCorrelative, int jobOrderNumberOfVisits, string jobOrderAgencyGenericCatalog, string jobOrderBrigadeGenericCatalog, string jobOrderClassificationGenericCatalog, string jobOrderTypeGenericCatalog, string jobOrderStatusGenericCatalog, string jobOrderReport, DateTime jobOrderCreateDate, string waterMeterMark, string waterMeterModel, string waterMeterDiameter, string waterMeterUnitMeasureGenericCatalog, Boolean waterMeterIsSpecial, decimal hisJobOrderReadingOne, decimal hisJobOrderReadingTwo)
+        {
+            WorkOrderId=workOrderId;
+            UserId=userId;
+            this.clientNumber=clientNumber;
+            this.actorIdCard=actorIdCard;
+            this.actorFullName=actorFullName;
+            this.actorPhone=actorPhone;
+            this.actorEmail=actorEmail;
+            this.actorAddress=actorAddress;
+            this.actorSecondaryAddress=actorSecondaryAddress;
+            this.jobOrderNumber=jobOrderNumber;
+            this.jobOrderApplicationDate=jobOrderApplicationDate;
+            this.jobOrderPath=jobOrderPath;
+            this.jobOrderCycle=jobOrderCycle;
+            this.jobOrderProperty=jobOrderProperty;
+            this.jobOrderTome=jobOrderTome;
+            this.jobOrderFolio=jobOrderFolio;
+            this.jobOrderCorrelative=jobOrderCorrelative;
+            this.jobOrderNumberOfVisits=jobOrderNumberOfVisits;
+            this.jobOrderAgencyGenericCatalog=jobOrderAgencyGenericCatalog;
+            this.jobOrderBrigadeGenericCatalog=jobOrderBrigadeGenericCatalog;
+            this.jobOrderClassificationGenericCatalog=jobOrderClassificationGenericCatalog;
+            this.jobOrderTypeGenericCatalog=jobOrderTypeGenericCatalog;
+            this.jobOrderStatusGenericCatalog=jobOrderStatusGenericCatalog;
+            this.jobOrderReport=jobOrderReport;
+            this.jobOrderCreateDate=jobOrderCreateDate;
+            this.waterMeterMark=waterMeterMark;
+            this.waterMeterModel=waterMeterModel;
+            this.waterMeterDiameter=waterMeterDiameter;
+            this.waterMeterUnitMeasureGenericCatalog=waterMeterUnitMeasureGenericCatalog;
+            this.waterMeterIsSpecial=waterMeterIsSpecial;
+            this.hisJobOrderReadingOne=hisJobOrderReadingOne;
+            this.hisJobOrderReadingTwo=hisJobOrderReadingTwo;
+        }
+
+        [Key]
+        public int WorkOrderId { get; set; }
+        public string UserId { get; set; }
+        public string clientNumber { get; set; }
+        public string actorIdCard { get; set; }
+        public string actorFullName { get; set; }
+        public string actorPhone { get; set; }
+        public string actorEmail { get; set; }
+        public string actorAddress { get; set; }
+        public string actorSecondaryAddress { get; set; }
+        public string jobOrderNumber { get; set; }
+        public DateTime jobOrderApplicationDate { get; set; }
+        public string jobOrderPath { get; set; }
+        public string jobOrderCycle { get; set; }
+        public string jobOrderProperty { get; set; }
+        public string jobOrderTome { get; set; }
+        public string jobOrderFolio { get; set; }
+        public int jobOrderCorrelative { get; set; }
+        public int jobOrderNumberOfVisits { get; set; }
+        public string jobOrderAgencyGenericCatalog { get; set; }
+        public string jobOrderBrigadeGenericCatalog { get; set; }
+        public string jobOrderClassificationGenericCatalog { get; set; }
+        public string jobOrderTypeGenericCatalog { get; set; }
+        public string jobOrderStatusGenericCatalog { get; set; }
+        public string jobOrderReport { get; set; }
+        public DateTime jobOrderCreateDate { get; set; }
+        public string waterMeterMark { get; set; }
+        public string waterMeterModel { get; set; }
+        public string waterMeterDiameter { get; set; }
+        public string waterMeterUnitMeasureGenericCatalog { get; set; }
+        public Boolean waterMeterIsSpecial { get; set; }
+        public decimal hisJobOrderReadingOne { get; set; }
+        public decimal hisJobOrderReadingTwo { get; set; }
+    }
+
+    public class SQLiteContext : DbContext
+    {
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=AriTracker.db", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Map table names
+            modelBuilder.Entity<WorkOrder>().ToTable("WorkOrder", "dbo");
+            modelBuilder.Entity<WorkOrder>(entity =>
+            {
+                entity.HasKey(e => e.WorkOrderId);
+                entity.Property(e => e.jobOrderApplicationDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
+                entity.Property(e => e.jobOrderCreateDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
     [ApiController]
     [Route("[controller]")]
     public class AriController : ControllerBase
@@ -21,59 +127,37 @@ namespace pruebaapiari.Controllers
         [HttpGet]
         public ListAriRequest Get(string userId)
         {
-            var listRequest = new List<AriRequest>();
+            using (var dbContext = new SQLiteContext())
+            {
+                return new ListAriRequest(dbContext.WorkOrders.Where(x => x.UserId == userId).ToList());
+            }
+        }
 
-            listRequest.Add(new AriRequest("OT-001", "Johnny Arcia", 1234, "Corte", userId));
-            listRequest.Add(new AriRequest("OT-002", "Byron Fonseca",2345,"Corte", userId));
-            listRequest.Add(new AriRequest("OT-003", "Test usuario", 3456, "Corte", userId));
-            listRequest.Add(new AriRequest("OT-004", "Test usuario", 3456, "Corte", userId));
-            listRequest.Add(new AriRequest("OT-005", "Test usuario", 3456, "Corte", userId));
-            listRequest.Add(new AriRequest("OT-006", "Test usuario", 3456, "Corte", userId));
-            listRequest.Add(new AriRequest("OT-007", "Test usuario", 3456, "Corte", userId));
-            listRequest.Add(new AriRequest("OT-008", "Test usuario", 3456, "Corte", userId));
-            listRequest.Add(new AriRequest("OT-009", "Test usuario", 3456, "Corte", userId));
-            listRequest.Add(new AriRequest("OT-010", "Test usuario", 3456, "Corte", userId));
-            listRequest.Add(new AriRequest("OT-011", "Test 1", 7891, "Reconexion", userId));
-            listRequest.Add(new AriRequest("OT-012", "Test 2", 7891, "Reconexion", userId));
-            listRequest.Add(new AriRequest("OT-013", "Test 3", 7891, "Reconexion", userId));
-            listRequest.Add(new AriRequest("OT-014", "Test 3", 7891, "Reconexion", userId));
-            listRequest.Add(new AriRequest("OT-015", "Test 3", 7891, "Reconexion", userId));
-            listRequest.Add(new AriRequest("OT-016", "Test 3", 7891, "Reconexion", userId));
-            listRequest.Add(new AriRequest("OT-017", "Test 3", 7891, "Reconexion", userId));
-            listRequest.Add(new AriRequest("OT-018", "Test 3", 7891, "Reconexion", userId));
-            listRequest.Add(new AriRequest("OT-019", "Test 3", 7891, "Reconexion", userId));
-            listRequest.Add(new AriRequest("OT-020", "Test 3", 7891, "Reconexion", userId));
-            return new ListAriRequest(listRequest);
+        [HttpPost]
+        public Boolean Post(WorkOrder request)
+        {
+            using (var dbContext = new SQLiteContext())
+            {
+                try
+                {
+                    dbContext.WorkOrders.Add(request);
+                    dbContext.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
         }
     }
     public class ListAriRequest
     {
-        public ListAriRequest(List<AriRequest> request) 
+        public ListAriRequest(List<WorkOrder> request) 
         {
             orders = request;
         }
-        public List<AriRequest> orders { get; set; }
+        public List<WorkOrder> orders { get; set; }
     }
 
-    public class AriRequest
-    {
-        public AriRequest(string _numeroOrden, string _nombre, int _numeroMedidor, string _tipoFlujo, string _idUsuario)
-        {
-            numeroOrden= _numeroOrden;
-            numeroMedidor = _numeroMedidor;
-            nombre= _nombre;
-            numeroMedidor = (int)_numeroMedidor;
-            tipoFlujo= _tipoFlujo;
-            idUsuario= _idUsuario;
-        }
-
-        public string numeroOrden { get; set; }
-
-        public string nombre { get; set; }
-
-        public int numeroMedidor { get; set; }
-
-        public string tipoFlujo { get; set; }
-        public string idUsuario { get; set; }
-    }
 }
--- pruebaapiari/pruebaapiari/Program.cs
+++ pruebaapiari/pruebaapiari/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using pruebaapiari.Controllers;
+using System.Reflection.Metadata;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,4 +26,28 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+Main();
+
 app.Run();
+
+static void Main()
+{
+    string dbName = "AriTracker.db";
+    if (File.Exists(dbName))
+    {
+        File.Delete(dbName);
+    }
+    using (var dbContext = new SQLiteContext())
+    {
+        dbContext.Database.EnsureCreated();
+        if (!dbContext.WorkOrders.Any())
+        {
+            dbContext.WorkOrders.AddRange(new WorkOrder[]
+            {
+                new WorkOrder(1,"userTest","1234","12345","Johnny Arcia","123451","jarcia@gmail.com","Test Address","Test second Arddress","OT-001",DateTime.Now,"2032","17","001","01",
+                "123",8000,1,"Agencia1","Brigada1","Clasificacion1","Corte","Asignado","",DateTime.Now,"BADGER","123","4","Pulgadas",false,1241223,233134)
+            });
+            dbContext.SaveChanges();
+        }
+    }
+}
